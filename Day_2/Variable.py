8fdc2ff633361524eb8cbbbbfff36066bd5789f1
Frankly <franklypoveda67@gmail.com>
2026-07-07 16:12:54 -0500

    day 2 complete

---
+++ Day_2/Variable.py
@@ -0,0 +1,90 @@
+#Day 2: 30 Days of Python Programming
+
+#Level 1
+first_name = "Frankly"
+last_Name = "Poveda"
+full_name = "Frankly Poveda"
+country = "Colombia"
+city = "Bucaramanga"
+age = 24
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False
+firstname, lastname, country1 = "Frankly", "Poveda", "Colombia"
+
+#Level 2
+print(type(first_name))
+print(type(last_Name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(firstname))
+print(type(lastname))
+print(type(country1))
+
+print(len(first_name))
+print(len(last_Name))
+
+num_one = 5
+num_two = 4
+total = sum([num_one, num_two])
+diff = num_two - num_one
+producto = num_one * num_two
+division = num_one / num_two
+risuduo = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+print("num_one:", num_one)
+print("num_two:", num_two)
+print("total:", total)
+print("difference:", diff)
+print("product:", producto)
+print("division:", division)
+print("remainder:", risuduo)
+print("exponent:", exp)
+print("floor division:", floor_division)
+
+
+r = 30
+area_of_circle = 3.1416 * (r**2)
+print("Area of circle:", area_of_circle)
+circum_of_circcle = 2 * 3.1416 * r
+print("Circumference of circle:", circum_of_circcle)
+print("Enter the area")
+R = float(input())
+area = 3.1416 + (R**2)
+print("Area with entered radius:", area)
+
+print("Enter your name")
+name = input()
+print("Enter your last name")
+lastname = input()
+print("Enter younr country")
+country = input()
+print("Enter your age")
+age =input()
+
+print("num_one:", num_one)
+print("num_two:", num_two)
+print("total:", total)
+print("difference:", diff)
+print("product:", producto)
+print("division:", division)
+print("remainder:", risuduo)
+print("exponent:", exp)
+print("floor division:", floor_division)
+print("Area of circle:", area_of_circle)
+print("Circumference of circle:", circum_of_circcle)
+print("Area with entered radius:", area)
+
+print("Name:", name)
+print("Last name:", lastname)
+print("Country:", country)
+print("Age:", age)
